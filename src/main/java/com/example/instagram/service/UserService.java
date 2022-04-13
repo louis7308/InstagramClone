32939589a3af62c33d844a7c5d37d0a12d659b6a
전승원 <jswa7308@naver.com>
2022-04-13 18:28:18 +0900

    :sparkles: UserService, UserRepository Settiing

---
+++ src/main/java/com/example/instagram/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.instagram.service;
+
+import com.example.instagram.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+// Bean등록 코드 @Component, @Service, @Controller, @Repository, @Bean, @Configuration
+
+@RequiredArgsConstructor // final 이나 @NonNull 인 필드 값만 파라미터로 받는 생성자를 만들어줍니다
+@Service
+public class UserService {
+
+    private final UserRepository userRepository; // final 은 선언부에서 초기화를 하지 않으면 안된다.
+    // 회원 정보 추가
+
+    // 선언적 트랜젝션 (메소드, 클래스, 인터페이스)
+    @Transactional // 트랜잭션 기능이 포함된 프록시 객체가 생성되어 자동으로 commit 혹은 rollback을 진행해준다.
+    public boolean save()
+}
