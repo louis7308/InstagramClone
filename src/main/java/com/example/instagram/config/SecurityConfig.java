d3a2739bfbcceac855f774a765e42076c7651f41
전승원 <jswa7308@naver.com>
2022-04-17 09:49:50 +0900

    :bug: :: starter-web 의존성 추가

--- src/main/java/com/example/instagram/config/SecurityConfig.java
+++ src/main/java/com/example/instagram/config/SecurityConfig.java
@@ -1,20 +1,21 @@
 package com.example.instagram.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @RequiredArgsConstructor // final @NonNull 키워드가 해당하는 값만 매개변수로 받아 생성자를 만든다.
 @EnableWebSecurity // 스프링 시큐리티 사용을 위한 어노테이션 선언
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurityConfigurerAdapter 상속
-
     /*
         * 스프링 시큐리티 규칙
     */
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // Cross site request foreign CSRF 공격방지
         http.authorizeRequests()
                 .antMatchers("/login", "/signup", "/style/**", "/js/**", "/img/**").permitAll() // antMathers 에는 권한 없이 접근을 허가해 줄 페이지 목록들을 적어
@@ -34,8 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurit
     /*
         * 스프링 시큐리티가 사용자를 인증하는 방법이 담긴 객체
      */
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) {
-
-    }
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) {
+//
+//    }
 }
